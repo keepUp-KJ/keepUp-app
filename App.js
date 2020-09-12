e6fdeda2d04c94b8838dda1f9e65194f5b1af08f
kmagued <kmagued@gmail.com>
2020-09-12 14:23:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,5 +2,7 @@ import React from "react";
 import Navigator from "./src/navigation/Navigator";
 
 export default function App() {
+  fetch("http://localhost:3000/api");
+
   return <Navigator />;
 }
